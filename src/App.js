50e6e3562a8cb6cf0c243735ce62bc011e300c1d
Leesahi Arie <leeshaiarie@gmail.com>
2025-06-22 19:52:12 +0300

    update users with json

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Jo",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+          };
+        })
+      );
   }
 
   render() {
     return (
-      <div className="App-header">
-        <p>Hi {this.state.name}</p>
-        <button
-          onClick={() => {
-            this.setState({ name: "Leo" });
-          }}
-        >
-          Change Name
-        </button>
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1 key={monster.id}>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
